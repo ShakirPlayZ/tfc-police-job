0cd7b607d7f96784322631617228a3843039b649
Manuel Höhn <mhoehn@live.de>
2023-02-06 15:29:20 +0100

    Removed unused Code

--- server/src/view.ts
+++ server/src/view.ts
@@ -10,29 +10,11 @@ import sync from "@AthenaClient/extensions/vehicleFuncs/sync";
 import {LocaleController} from "@AthenaShared/locale/locale";
 import {LOCALE_KEYS} from "@AthenaShared/locale/languages/keys";
 
-const InternalFuncs = {
-    reanimateplayer(player: alt.Player, id: string) {
-        alt.log("REANIMATE player: ", player.name);
-        alt.log("REANIMATE target: ", id);
-
-        const target = Athena.systems.identifier.getPlayer(id);
-        if (!target) {
-            Athena.player.emit.message(player, LocaleController.get(LOCALE_KEYS.CANNOT_FIND_PLAYER));
-            return;
-        }
-        if (!target.data.isDead) {
-            return;
-        }
-        Athena.player.set.respawned(target, target.pos);
-    },
-}
 
 export class TfcPoliceWebViewServer {
     static init() {
         console.log(`Commands for TFC PD Job Created`);
         alt.onClient(TfcPoliceWebViewEvents.WebViewToServer.REQUEST_REFRESH, TfcPoliceWebViewServer.handleRefresh);
-
-        alt.onClient(TFCPOLICE_EVENTS.TO_SERVER.REANIMATEPLAYER, InternalFuncs.reanimateplayer);
     }
 
     @command('pdjob', '/pdjob - Job Menü anzeigen', PERMISSIONS.ADMIN | PERMISSIONS.MODERATOR | CHARACTER_PERMISSIONS.LSPD)
